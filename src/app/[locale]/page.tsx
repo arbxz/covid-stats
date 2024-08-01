5556b190bb0fda62e9a825eecdbadc24477b5f93
Arbaaz Mowlabucus <91611231+arbxz@users.noreply.github.com>
2024-08-01 21:45:04 +0400

    Minor style modifications + landing page setup

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,98 +1,107 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
+import { Activity, ArrowRight, Database } from "lucide-react";
+import { formatNumber } from "@/utils/utils";
 import LocaleSwitcher from "@/components/locale-switch/LocaleSwitcher";
 import { ModeToggle } from "@/components/theme-toggle";
-import { useTranslations } from "next-intl";
-import Image from "next/image";
+
+// TODO remove unused imports
+// TODO fix imports sorting
+// TODO Add a comment to describe the function
+// TODO figure out where to place util functions
+// TODO convert cards into components
 
 export default function Home() {
   const t = useTranslations("HomePage");
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          {t("title")}
+    <main className="flex flex-col min-h-screen p-4 md:p-8">
+      <nav className="flex items-center justify-between gap-4 w-full">
+        <div className="group bg-custom-primary text-white dark:bg-primary-foreground border-custom-primary flex gap-4 items-center px-4 py-2 border-[1px] rounded-full">
+          <Activity size={16} />
+          <span className="text-sm">covid-stats</span>
+        </div>
+        <ul className="hidden lg:flex items-center gap-4 text-sm">
+          <li>
+            <a
+              className="py-2 hover:pb-4 border-custom-primary border-b-[1px] hover:border-b-2 transition-all duration-300"
+              href="/about">
+              Active cases
+            </a>
+          </li>
+          <li>
+            <a
+              className="py-2 hover:pb-4 border-custom-primary border-b-[1px] hover:border-b-2 transition-all duration-300"
+              href="/about">
+              Vaccination data
+            </a>
+          </li>
+        </ul>
+        <div className="flex items-center gap-4">
           <LocaleSwitcher />
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <ModeToggle />
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+      </nav>
+      <div className="relative overflow-hidden max-w-7xl mx-auto flex-1 flex flex-col md:flex-row items-center justify-center gap-8 lg:gap-16 py-4">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
+          className="absolute hidden lg:block top-1/2 left-1/3 -translate-x-1/2 -translate-y-1/2 -rotate-12 dark:drop-shadow-[0_0_0.3rem_#ffffff70] "
+          src="/chart.svg"
+          width={950}
+          height={600}
           alt="Next.js Logo"
-          width={180}
-          height={37}
           priority
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer">
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="relative w-full text-center md:text-left py-8">
+          <h1 className="font-semibold text-4xl lg:text-6xl mb-4 lg:mb-8">
+            {t("title")}
+          </h1>
+          <p className="text-md md:text-lg mb-4">{t("description")}</p>
+          <div className="lg:text-right">
+            <a
+              className="px-4 py-1 border-custom-primary text-custom-primary hover:shadow-md transition-all duration-500 border-[1px] rounded-full text-xs"
+              href="https://disease.sh"
+              target="_blank">
+              {t("dataset")}
+              <Database className="inline-block ml-2" size={12} />
+            </a>
+          </div>
+        </div>
+        <div className="relative z-10 flex flex-col justify-center items-center gap-4 w-full md:w-1/3 lg:w-72">
+          {/* Card */}
+          <div className="group text-center hover:border-custom-primary transition-colors duration-300 bg-white dark:bg-primary-foreground border-[1px] rounded-md">
+            <div className="flex flex-col justify-center items-center p-8">
+              <span className="text-4xl font-semibold mb-4">
+                {formatNumber(22123398)}
+              </span>
+              <span>Active cases</span>
+            </div>
+            <div className="p-4 group-hover:border-custom-primary border-t-[1px] transition-colors duration-300">
+              <Link
+                href={""}
+                className="flex justify-start items-end text-xs text-left text-slate-500">
+                View active covid cases datasets.
+                <ArrowRight className="inline ml-2" size={14} />
+              </Link>
+            </div>
+          </div>
+          <div className="group text-center hover:border-custom-primary transition-colors duration-300 bg-white dark:bg-primary-foreground border-[1px] rounded-md">
+            <div className="flex flex-col justify-center items-center p-8">
+              <span className="text-4xl font-semibold mb-4">
+                {formatNumber(22123398)}
+              </span>
+              <span>Active cases</span>
+            </div>
+            <div className="p-4 group-hover:border-custom-primary border-t-[1px] transition-colors duration-300">
+              <Link
+                href={""}
+                className="flex justify-start items-end text-xs text-left text-slate-500">
+                View active covid cases datasets.
+                <ArrowRight className="inline ml-2" size={14} />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
--- src/components/locale-switch/LocaleSwitcher.tsx
+++ src/components/locale-switch/LocaleSwitcher.tsx
@@ -5,15 +5,5 @@ export default function LocaleSwitcher() {
   const t = useTranslations("LocaleSwitcher");
   const locale = useLocale();
 
-  return (
-    // <LocaleSwitcherSelect defaultValue={locale} label={t("label")}>
-    //   {locales.map((cur) => (
-    //     <SelectItem key={cur} value={cur}>
-    //       {t("locale", { locale: cur })}
-    //     </SelectItem>
-    //   ))}
-    // </LocaleSwitcherSelect>
-
-    <LocaleSwitcherSelect defaultValue={locale} label={t("label")} />
-  );
+  return <LocaleSwitcherSelect defaultValue={locale} label={t("label")} />;
 }
